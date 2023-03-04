ec490dfb0967046c79f86399f1d32ceab5d7cc38
reymagos <reymagos838@gmail.com>
2023-03-05 00:31:37 +0500

    Line Controller

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { render } from "preact"
 import { App } from "./app"
 import "./index.less"
 import {TemplatesProvider} from "./templates/provider";
-import {CategoryScale, Chart, Legend, LinearScale, LineElement, ScatterController, PointElement, Tooltip} from "chart.js";
+import {CategoryScale, Chart, Legend, LinearScale, LineElement, ScatterController, LineController, PointElement, Tooltip} from "chart.js";
 import {ProcessController} from "./process/controller";
 import {Config} from "src/config";
 
-Chart.register(ScatterController, CategoryScale, LinearScale, PointElement, LineElement, Legend, Tooltip)
+Chart.register(ScatterController, LineController, CategoryScale, LinearScale, PointElement, LineElement, Legend, Tooltip)
 
 TemplatesProvider.init()
 ProcessController.init()
